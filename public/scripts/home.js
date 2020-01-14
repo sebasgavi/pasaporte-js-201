4e1f768a75bfc7caeb5756d65f9f99ee61372b88
Sebastián Gaviria <vagyriag@hotmail.com>
2020-01-14 16:01:49 -0500

    cards con columnas

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -9,16 +9,21 @@ fetch('/api/1/movies')
     .then(function(json){
         console.log(json);
         json.forEach(function(movie, index){
+            const col = document.createElement('div');
+            col.classList.add('col-4');
+            
             const article = document.createElement('article');
             article.classList.add('card');
             article.innerHTML = `
                 <img src="https://source.unsplash.com/random/300x200?v=${movie._id}" class="card-img-top" alt="">
                 <div class="card-body">
                     <h5 class="card-title">${movie.original_title}</h5>
-                    <p class="card-text">${movie.overview}</p>
+                    <p class="card-text">${movie.overview.substring(0, 150)}</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             `;
-            container.appendChild(article);
+
+            col.appendChild(article);
+            container.appendChild(col);
         });
     })
