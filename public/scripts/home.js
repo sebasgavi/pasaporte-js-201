976dcb87ea9eaae90d004d689f52addb4959a88e
Sebastián Gaviria <vagyriag@hotmail.com>
2020-01-14 15:51:02 -0500

    cards con contenido e imágenes

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -11,7 +11,14 @@ fetch('/api/1/movies')
         json.forEach(function(movie, index){
             const article = document.createElement('article');
             article.classList.add('card');
-            article.innerHTML = movie.original_title;
+            article.innerHTML = `
+                <img src="https://source.unsplash.com/random/300x200?v=${movie._id}" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">${movie.original_title}</h5>
+                    <p class="card-text">${movie.overview}</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            `;
             container.appendChild(article);
         });
     })
